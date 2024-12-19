f017ecb94ffa4c79ecc17d6eff53e69a20904818
harshita795 <yadavharshita261303@gmail.com>
2024-12-19 21:07:55 +0530

    feat: if-else in python

--- PY_1/app.py
+++ PY_1/app.py
@@ -401,6 +401,82 @@ def check_rating():
     result = 'high'
   return f"Restaurant rating is {result}"
 
+# PY_1.3_HW_2 (if-Else)
+@app.route("/check-bmi", methods=["GET"])
+def check_bmi():
+  height = float(request.args.get("height", 1)) 
+  weight = float(request.args.get("weight", 0))
+  bmi =  weight / (height * height) 
+  if bmi < 18.5:
+    result = "under weight"
+  elif bmi >= 18.5 and bmi <= 24.9:
+    result = "normal weight"
+  elif bmi >= 25.0 and bmi <= 29.9:
+    result = "over weight"
+  else:
+    result = "obesity"
+  return f"BMI category is {result}"
+
+@app.route("/check-performance", methods=["GET"])
+def check_performance():
+  grade = float(request.args.get("grade", 0))
+  if grade >= 90:
+    result = 'excellent'
+  elif grade >= 75:
+    result = 'good'
+  elif grade >= 50:
+    result = 'average'
+  else:
+    result = 'poor'
+  return f"Academic performance is {result}"
+
+@app.route("/check-age-group", methods=["GET"])
+def check_age_group():
+  age = float(request.args.get("age", 0))
+  if age <= 12:
+    result = 'child'
+  elif age <= 17:
+    result = 'teenager'
+  elif age <= 64:
+    result = 'adult'
+  else:
+    result = 'senior'
+  return f"Age group is {result}"
+
+@app.route("/check-loan-eligibility", methods=["GET"])
+def check_loan_eligibility():
+  creditScore = float(request.args.get("creditScore", 0))
+  if creditScore >= 750:
+    result = 'high'
+  elif creditScore >= 650:
+    result = 'medium'
+  else:
+    result = 'low'
+  return f"Loan eligibility is {result}"
+
+@app.route("/check-tax-bracket", methods=["GET"])
+def check_tax_bracket():
+  income = float(request.args.get("income", 0))
+  if income <= 500000:
+    result = '10% tax bracket'
+  elif income <= 1000000:
+    result = '15% tax bracket'
+  elif income <= 1500000:
+    result = '20% tax bracket'
+  else:
+    result = '30% tax bracket'
+  return f"You fall under the {result}"
+
+@app.route("/check-experiences", methods=["GET"])
+def check_experiences():
+  yearsOfExperience = float(request.args.get("yearsOfExperience", 0))
+  if yearsOfExperience > 5:
+    result = "expert"
+  else:
+    result = "Noob"
+  return f"Experience level is {result}"
+
+
 
 if __name__ == "__main__":
   app.run()
